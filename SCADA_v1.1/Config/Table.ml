c98f64fd921b1303f449d409b045d6b2d6331c47
Tiago Cunha <up202007672@up.pt>
2024-04-01 01:11:59 +0100

    Falta atualizar as gates do SCADA

---
+++ SCADA_v1.1/Config/Table.ml
@@ -0,0 +1,86 @@
+§
+Language0§Language1
+Conveyor1§Conveyor1
+Conveyor2§Conveyor2
+Conveyor3§Conveyor3
+conveyor1§conveyor1
+conveyor2§conveyor2
+conveyor3§conveyor3
+sensor1§sensor1
+sensor2§sensor2
+STOP 1§STOP 1
+Conveyor 1 overloaded!§Conveyor 1 overloaded!
+Conveyor 2 Overload!§Conveyor 2 Overload!
+Alarms§Alarms
+Active§Active
+Ack§Ack
+Message§Message
+Date§Date
+Time§Time
+Class 1§Class 1
+Class 2§Class 2
+To ack§To ack
+Excluded§Excluded
+Alarm History§Alarm History
+Start date§Start date
+Start time§Start time
+End date§End date
+End time§End time
+Conveyor 1 switched on.§Conveyor 1 switched on.
+Conveyor 2 switched on.§Conveyor 2 switched on.
+Events§Events
+Close§Close
+close§close
+Button§Button
+Blink ON§Blink ON
+Blink OFF§Blink OFF
+Conveyor4§Conveyor4
+Conv1§Conv1
+Conveyor11§Conveyor11
+Conveyor111§Conveyor111
+Conveyor1111§Conveyor1111
+Conv3§Conv3
+Conv4§Conv4
+Conv5§Conv5
+Conv6§Conv6
+Conv7§Conv7
+Conv2§Conv2
+Conv8§Conv8
+Manual§Manual
+Maintenance§Maintenance
+Automatic§Automatic
+Stop§Stop
+Conv9§Conv9
+Conv10§Conv10
+Conv11§Conv11
+Conv12§Conv12
+Conv13§Conv13
+Sensor 1§Sensor 1
+Sensor 3§Sensor 3
+Sensor 4§Sensor 4
+Sensor 5§Sensor 5
+Sensor 6§Sensor 6
+Sensor 7§Sensor 7
+Sensor 2§Sensor 2
+Sensor 8§Sensor 8
+Sensor 9§Sensor 9
+Sensor 10§Sensor 10
+Sensor 11§Sensor 11
+Sensor 12§Sensor 12
+Sensor 13§Sensor 13
+Conveyor 1§Conveyor 1
+Conveyor 2§Conveyor 2
+Conveyor 3§Conveyor 3
+Conveyor 4§Conveyor 4
+Conveyor 5§Conveyor 5
+Conveyor 6§Conveyor 6
+Conveyor 7§Conveyor 7
+Conveyor 8§Conveyor 8
+Conveyor 9§Conveyor 9
+Conveyor 10§Conveyor 10
+Conveyor 11§Conveyor 11
+Conveyor 12§Conveyor 12
+Conveyor 13§Conveyor 13
+Auto§Auto
+Motor-§Motor-
+Motor+§Motor+
